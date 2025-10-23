ed71c1d04e96dcb2db5583d80141e16e5fa148e0
Alnagdi Mohsen <209276428+Adam-Alhakam@users.noreply.github.com>
2025-10-23 21:56:08 +0000

    Added product model, templates, and views

---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('products/', views.product_list_view, name='product-list'),
+]
